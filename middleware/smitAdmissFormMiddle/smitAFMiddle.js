93f0ab81dc740eae949ce57638d43eee714b3823
mumairkhan1997 <mohumairkhan47@gmail.com>
2021-01-03 10:44:20 +0500

    sunday-pushes

--- middleware/smitAdmissFormMiddle/smitAFMiddle.js
+++ middleware/smitAdmissFormMiddle/smitAFMiddle.js
@@ -1,7 +1,20 @@
-const autoIncrement = require('mongoose-auto-increment');
-const SmitAdmissFormModel = require("../../model/SmitModel/SmitAdmissForm");
+// const autoIncrement = require('mongoose-auto-increment');
+// const SmitAdmissFormModel = require("../../model/SmitModel/SmitAdmissForm");
 
-// startIdPass = (req, res, next) => {
+// // startIdPass = (req, res, next) => {
+// //     const { id } = req.body
+// //     SmitAdmissFormModel.plugin(autoIncrement.plugin, {
+// //         model: 'user',
+// //         field: '_id',
+// //         startAt: id,
+// //         incrementBy: 1
+// //     });
+// // };
+
+// // module.exports = { startIdPass }
+
+
+// module.exports = (req, res, next) => {
 //     const { id } = req.body
 //     SmitAdmissFormModel.plugin(autoIncrement.plugin, {
 //         model: 'user',
@@ -9,18 +22,5 @@ const SmitAdmissFormModel = require("../../model/SmitModel/SmitAdmissForm");
 //         startAt: id,
 //         incrementBy: 1
 //     });
+//     next();
 // };
-
-// module.exports = { startIdPass }
-
-
-module.exports = (req, res, next) => {
-    const { id } = req.body
-    SmitAdmissFormModel.plugin(autoIncrement.plugin, {
-        model: 'user',
-        field: '_id',
-        startAt: id,
-        incrementBy: 1
-    });
-    next();
-};
