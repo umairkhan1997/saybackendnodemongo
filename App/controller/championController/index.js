d4930fff2b5193787abab80b19367942f2902910
mumairkhan1997 <mohumairkhan47@gmail.com>
2021-06-08 01:34:35 +0500

    champions-updated

--- App/controller/championController/index.js
+++ App/controller/championController/index.js
@@ -5,7 +5,7 @@ const championsModel = require("../../model/championsModel/index");
 championGet = async (req, res) => {
     try {
         const result = await getAllData('championMd')
-        return res.status(200).send({ status: 200, success: true, message: 'The data is fetch successfully .', result })
+        return res.status(200).send({ status: 200, success: true, message: 'The data is fetch successfully.', result })
     } catch (e) {
         return res.status(200).send({ status: 200, message: e.message })
     }
